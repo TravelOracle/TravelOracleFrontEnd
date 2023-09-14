de18a856cc22b4761170eecfd71f8b5e9bfe02e5
TheoThibodeau <teddythibodeau@gmail.com>
2023-09-14 15:54:05 -0400

    Buttons added and some css

--- TravelAIFE/src/components/filters.jsx
+++ TravelAIFE/src/components/filters.jsx
@@ -31,27 +31,78 @@ const Filters = () => {
         console.log(selectedEnvironment)
     }
 
+    const handleInterest = (selectedInterest) => {
+        setInterest(selectedInterest)
+        console.log(selectedInterest)
+    }
+
+    const handleTravelBudget = (selectedTravelBudget) => {
+        setTravelBudget(selectedTravelBudget)
+        console.log(selectedTravelBudget)
+    }
+
+    const handleHome = (selectedHome) => {
+        setHome(selectedHome)
+        console.log(selectedHome)
+    }
+
+    const handleClimate = (selectedClimate) => {
+        setClimate(selectedClimate)
+        console.log(selectedClimate)
+    }
+
+    const handleTravelGroup = (selectedTravelGroup) => {
+        setTravelGroup(selectedTravelGroup)
+        console.log(selectedTravelGroup)
+    }
+
+    const handleContinent = (selectedContinent) => {
+        setContinent(selectedContinent)
+        console.log(selectedContinent)
+    }
+
     return(
         <>
         <div>
-            <h1>Travel Oracle</h1>
-            <h3>Environment</h3>
-                <button onClick={() => handleEnvironment("Coastal")}>Coastal</button>
-                <button onClick={() => handleEnvironment("Mountains")}>Mountains</button>
-                <button onClick={() => handleEnvironment("Urban")}>Urban</button>
-            <h3>Continent/Area</h3>
-                <button onClick={() => handleEnvironment("Europe")}>Europe</button>
-                <button>Asia</button>
-                <button>Africa</button>
-                <button>South America</button>
-                <button>Middle East</button>
-                <button>Caribbean</button>
-                <button>South East Asia</button>
-            <button onClick={handlePost}>Generate</button>
+            <h1 class="appTitle">Travel Oracle</h1>
+            <h3 class="filterTitle">Environment</h3>
+                <button class="button-51" role="button" onClick={() => handleEnvironment("Coastal")}>Coastal</button>
+                <button class="button-51" role="button" onClick={() => handleEnvironment("Mountains")}>Mountains</button>
+                <button class="button-51" role="button" onClick={() => handleEnvironment("Urban")}>Urban</button>
+                <button class="button-51" role="button" onClick={() => handleEnvironment("Countryside")}>Countryside</button>
+                <button class="button-51" role="button" onClick={() => handleEnvironment("Arid")}>Arid</button>
+                <button class="button-51" role="button" onClick={() => handleEnvironment("Forest")}>Forest</button>
+                <button class="button-51" role="button" onClick={() => handleEnvironment("Tropical")}>Tropical</button>
+            <h3 class="filterTitle">Continent/Area</h3>
+                <button class="button-51" role="button" onClick={() => handleContinent("Europe")}>Europe</button>
+                <button class="button-51" role="button" onClick={() => handleContinent("Asia")}>Asia</button>
+                <button class="button-51" role="button" onClick={() => handleContinent("Africa")}>Africa</button>
+                <button class="button-51" role="button" onClick={() => handleContinent("South America")}>South America</button>
+                <button class="button-51" role="button" onClick={() => handleContinent("Middle East")}>Middle East</button>
+                <button class="button-51" role="button" onClick={() => handleContinent("Caribbean")}>Caribbean</button>
+                <button class="button-51" role="button" onClick={() => handleContinent("South East Asia")}>South East Asia</button>
+            <h3 class="filterTitle">Budget</h3>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("Under $200")}>Under $200</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$201-$500")}>$201-$500</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$501-$1,000")}>$501-$1,000</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$1,001-$2,000")}>$1,001-$2,000</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$2,001-$4,000")}>$2,001-$4,000</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$4,001-$7,000")}>$4,001-$7,000</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$7,001-$10,000")}>$7,001-$10,000</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$10,001-$14,000")}>$10,001-$14,000</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("$14,001-$20,000")}>$14,001-$20,000</button>
+                <button class="button-51" role="button" onClick={() => handleTravelBudget("Money is no object")}>Money is no object</button>
+            <h3 class="filterTitle">Interests</h3>
+                <button class="button-51" role="button" onClick={() => handleInterest("Beach")}>Beach</button>
+                <button class="button-51" role="button" onClick={() => handleInterest("Historical Sites")}>Historical Sites</button>
+                <button class="button-51" role="button" onClick={() => handleInterest("Restaurants")}>Restaurants</button>
+                <button class="button-51" role="button" onClick={() => handleInterest("Wine Tasting")}>Wine Tasting</button>
+                <button class="button-51" role="button" onClick={() => handleInterest("Outdoor Activities")}>Outdoor Activities</button>
+                <button class="button-51" role="button" onClick={() => handleInterest("Sports")}>Sports</button>
+            <h3> </h3>    
+            <button class="button-74" role="button" onClick={handlePost}>Generate</button>
         </div>
         </>
     )
-
-
 }
 export default Filters
