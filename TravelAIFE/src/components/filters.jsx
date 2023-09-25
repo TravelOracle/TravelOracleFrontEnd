23d4b4fadc21b56a3d7710b1f7141651ab6f1241
TheoThibodeau <teddythibodeau@gmail.com>
2023-09-25 12:30:38 -0400

    post request 1

--- TravelAIFE/src/components/filters.jsx
+++ TravelAIFE/src/components/filters.jsx
@@ -5,7 +5,7 @@ import Dropdown from "./dropdown";
 
 const Filters = () => {
     const [open, setOpen] = React.useState(false);
-    const [environment, setEnvironment] = useState("Mountains");
+    const [environment, setEnvironment] = useState("");
     const [interest, setInterest] = useState("");
     const [travelBudget, setTravelBudget] = useState("");
     const [home, setHome] = useState("");
@@ -88,7 +88,7 @@ const Filters = () => {
     return(
         <>
         <div>
-            <h1 class="appTitle">Travel Oracle</h1>
+            <h1 className="appTitle">Travel Oracle</h1>
         <Dropdown
             open={open}
             trigger={<button onClick={handleOpen}>Environment</button>}
@@ -119,34 +119,50 @@ const Filters = () => {
                  <br />
                  <br />
 
-            <h3 class="filterTitle">Continent/Area</h3>
-                <button class="button-85" role="button" onClick={() => handleContinent("Europe")}>Europe</button>
-                <button class="button-85" role="button" onClick={() => handleContinent("Asia")}>Asia</button>
-                <button class="button-85" role="button" onClick={() => handleContinent("Africa")}>Africa</button>
-                <button class="button-85" role="button" onClick={() => handleContinent("South America")}>South America</button>
-                <button class="button-85" role="button" onClick={() => handleContinent("Middle East")}>Middle East</button>
-                <button class="button-85" role="button" onClick={() => handleContinent("Caribbean")}>Caribbean</button>
-                <button class="button-85" role="button" onClick={() => handleContinent("South East Asia")}>South East Asia</button>
-            <h3 class="filterTitle">Budget</h3>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("Under $200")}>Under $200</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$201-$500")}>$201-$500</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$501-$1,000")}>$501-$1,000</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$1,001-$2,000")}>$1,001-$2,000</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$2,001-$4,000")}>$2,001-$4,000</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$4,001-$7,000")}>$4,001-$7,000</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$7,001-$10,000")}>$7,001-$10,000</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$10,001-$14,000")}>$10,001-$14,000</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("$14,001-$20,000")}>$14,001-$20,000</button>
-                <button class="button-85" role="button" onClick={() => handleTravelBudget("Money is no object")}>Money is no object</button>
-            <h3 class="filterTitle">Interests</h3>
-                <button class="button-85" role="button" onClick={() => handleInterest("Beach")}>Beach</button>
-                <button class="button-85" role="button" onClick={() => handleInterest("Historical Sites")}>Historical Sites</button>
-                <button class="button-85" role="button" onClick={() => handleInterest("Restaurants")}>Restaurants</button>
-                <button class="button-85" role="button" onClick={() => handleInterest("Wine Tasting")}>Wine Tasting</button>
-                <button class="button-85" role="button" onClick={() => handleInterest("Outdoor Activities")}>Outdoor Activities</button>
-                <button class="button-85" role="button" onClick={() => handleInterest("Sports")}>Sports</button>
-            <h3> </h3>    
-            <button class="button-74" role="button" onClick={handlePost}>Generate</button>
+            <h3 className="filterTitle">Continent/Area</h3>
+                <button className="button-85" role="button" onClick={() => handleContinent("Europe")}>Europe</button>
+                <button className="button-85" role="button" onClick={() => handleContinent("Asia")}>Asia</button>
+                <button className="button-85" role="button" onClick={() => handleContinent("Africa")}>Africa</button>
+                <button className="button-85" role="button" onClick={() => handleContinent("South America")}>South America</button>
+                <button className="button-85" role="button" onClick={() => handleContinent("Middle East")}>Middle East</button>
+                <button className="button-85" role="button" onClick={() => handleContinent("Caribbean")}>Caribbean</button>
+                <button className="button-85" role="button" onClick={() => handleContinent("South East Asia")}>South East Asia</button>
+            <h3 className="filterTitle">Budget</h3>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("Under $200")}>Under $200</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$201-$500")}>$201-$500</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$501-$1,000")}>$501-$1,000</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$1,001-$2,000")}>$1,001-$2,000</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$2,001-$4,000")}>$2,001-$4,000</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$4,001-$7,000")}>$4,001-$7,000</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$7,001-$10,000")}>$7,001-$10,000</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$10,001-$14,000")}>$10,001-$14,000</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("$14,001-$20,000")}>$14,001-$20,000</button>
+                <button className="button-85" role="button" onClick={() => handleTravelBudget("Money is no object")}>Money is no object</button>
+            <h3 className="filterTitle">Interests</h3>
+                <button className="button-85" role="button" onClick={() => handleInterest("Beach")}>Beach</button>
+                <button className="button-85" role="button" onClick={() => handleInterest("Historical Sites")}>Historical Sites</button>
+                <button className="button-85" role="button" onClick={() => handleInterest("Restaurants")}>Restaurants</button>
+                <button className="button-85" role="button" onClick={() => handleInterest("Wine Tasting")}>Wine Tasting</button>
+                <button className="button-85" role="button" onClick={() => handleInterest("Outdoor Activities")}>Outdoor Activities</button>
+                <button className="button-85" role="button" onClick={() => handleInterest("Sports")}>Sports</button>
+            <h3 className="filterTitle">Home</h3>    
+                <button className="button-85" role="button" onClick={() => handleHome("Wilmington")}>Wilmington</button>
+                <button className="button-85" role="button" onClick={() => handleHome("San Diego")}>San Diego</button>
+                <button className="button-85" role="button" onClick={() => handleHome("Mexico City")}>Mexico City</button>
+                <button className="button-85" role="button" onClick={() => handleHome("Perth")}>Perth</button>
+                <button className="button-85" role="button" onClick={() => handleHome("Toronto")}>Toronto</button>
+                <button className="button-85" role="button" onClick={() => handleHome("Istanbul")}>Istanbul</button>
+            <h3 className="filterTitle">Climate</h3>
+                <button className="button-85" role="button" onClick={() => handleClimate("Arid")}>Arid</button>
+                <button className="button-85" role="button" onClick={() => handleClimate("Rainy")}>Rainy</button>
+                <button className="button-85" role="button" onClick={() => handleClimate("Cold")}>Cold</button>
+                <button className="button-85" role="button" onClick={() => handleClimate("Sunny")}>Sunny</button>
+            <h3 className="filterTitle">Travel Group</h3>
+                <button className="button-85" role="button" onClick={() => handleTravelGroup("Solo")}>Solo</button>
+                <button className="button-85" role="button" onClick={() => handleTravelGroup("2 people")}>2 people</button>
+                <button className="button-85" role="button" onClick={() => handleTravelGroup("3-4 people")}>3-4 people</button>
+                <button className="button-85" role="button" onClick={() => handleTravelGroup("5 or more")}>5 or more</button>
+            <button className="button-74" role="button" onClick={handlePost}>Generate</button>
         </div>
         </>
     )
